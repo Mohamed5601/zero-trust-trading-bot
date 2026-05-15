697a335614c202b3008b14e323c39e5e59949306
Mohamed5601 <mada4605123@gmail.com>
2026-05-16 00:43:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ SYMBOLS = [
 ]
 
 # --- 1. إعدادات التليجرام ---
-TELEGRAM_TOKEN = "8464093213:AAGUE7q_ZdzeSLC_INsgR35bvWa8HkfrB9s"
-TELEGRAM_CHAT_ID = "-1003484158830"
+TELEGRAM_TOKEN = ""
+TELEGRAM_CHAT_ID = ""
 
 # الفاصل الزمني بين كل سحبة بيانات (بالثواني)
 # 300 ثانية = 5 دقائق
@@ -22,10 +22,10 @@ DB_NAME = 'market_data.db'
 
 # --- إعدادات الأخبار والذكاء الاصطناعي ---
 # مفتاح CryptoPanic (المصدر)
-CRYPTOPANIC_TOKEN = "c23a5832dd7ce08bcfa3cc4fab5ccbfe6eedd23c"
+CRYPTOPANIC_TOKEN = ""
 
 
-GEMINI_API_KEY = "AIzaSyCvj0u-uHrbUc8B3nbR0KSux-nzTDkfY9g"
+GEMINI_API_KEY = ""
 
 # كلمات مفتاحية لو الخبر احتوى عليها نعتبره هام جداً (اختياري)
 IMPORTANT_KEYWORDS = ['Binance', 'SEC', 'ETF', 'Rate Cut', 'Inflation', 'Powell']
@@ -33,4 +33,4 @@ IMPORTANT_KEYWORDS = ['Binance', 'SEC', 'ETF', 'Rate Cut', 'Inflation', 'Powell'
 # ضع مفاتيح Bybit هنا
 BINANCE_API_KEY = "مفتاح_باي_بت_الجديد"    # سميناها BINANCE في الكود القديم، عادي اترك الاسم كما هو وضع القيمة الجديدة
 BINANCE_SECRET_KEY = "مفتاح_السر_الجديد"
-TRADE_AMOUNT_USDT = 15
+TRADE_AMOUNT_USDT = 500
